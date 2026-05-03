338cd14aa0aed33cd83717670b61b69ad85d44a0
spix367 <gurami.miqeladze.20@gmail.com>
2026-05-03 21:52:28 +0400

    first commit

---
+++ aplication/views.py
@@ -0,0 +1,11 @@
+
+from django.shortcuts import render
+
+def base(request):
+    return render(request, 'base.html')
+def about(request):
+    return render(request, 'about.html')
+def contact(request):
+    return render(request, 'contact.html')
+def services(request):
+    return render(request, 'services.html')
